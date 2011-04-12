8de3346e2298affbfd5de6cf708feb00677de636
luzi82 <luzi82@gmail.com>
2011-04-12 21:44:56 +0800

    fix timer update time and time cal
    issue #7

--- src/com/luzi82/madokacountdown/CountdownAppWidgetProvider.java
+++ src/com/luzi82/madokacountdown/CountdownAppWidgetProvider.java
@@ -15,23 +15,14 @@ public class CountdownAppWidgetProvider extends AppWidgetProvider {
 	static long mDeadline;
 	static {
 		GregorianCalendar deadline = new GregorianCalendar(TimeZone.getTimeZone("GMT+09"));
-		// GregorianCalendar deadline = new GregorianCalendar();
 		deadline.set(2011, Calendar.APRIL, 22, 2, 40);
 		mDeadline = deadline.getTime().getTime();
 	}
 
-	// @Override
-	// public void onEnabled(Context context) {
-	// super.onEnabled(context);
-	//
-	// Intent i = new Intent(context, MainService.class);
-	// context.startService(i);
-	// }
-
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
-		doUpdate(context, appWidgetManager, appWidgetIds);
+		doUpdate(context, appWidgetManager, appWidgetIds, System.currentTimeMillis());
 
 		Intent i = new Intent(context, MainService.class);
 		context.startService(i);
@@ -41,13 +32,8 @@ public class CountdownAppWidgetProvider extends AppWidgetProvider {
 		context.sendBroadcast(updateIntent);
 	}
 
-	public static void doUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		// GregorianCalendar nn = new
-		// GregorianCalendar(TimeZone.getTimeZone("UTC+08"));
-		GregorianCalendar nn = new GregorianCalendar();
-		long now = nn.getTime().getTime();
-		// long now = System.currentTimeMillis();
-		int diff = (int) (mDeadline - now);
+	public static void doUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds, long now) {
+		int diff = (int) (mDeadline - (now + 500));
 		diff /= 1000;
 
 		int sec = diff % 60;
@@ -58,20 +44,7 @@ public class CountdownAppWidgetProvider extends AppWidgetProvider {
 		diff /= 24;
 		int day = diff;
 
-		// StringBuffer sb = new StringBuffer();
-		// sb.append();
-		// // sb.append(day);
-		// // sb.append(",");
-		// // sb.append(hr);
-		// // sb.append(":");
-		// // sb.append(min);
-		// // sb.append(":");
-		// // sb.append(sec);
-		// // sb.append(diff);
-		// // sb.append("s");
 		String s = String.format("%d日 %02d:%02d:%02d", day, hr, min, sec);
-
-		// Log.d("CountdownAppWidgetProvider", s);
 
 		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget);
 		views.setTextViewText(R.id.title, s);
