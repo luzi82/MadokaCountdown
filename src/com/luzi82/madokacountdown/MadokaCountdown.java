29ced265d5ffe0cb24241d9db602264c89a4fcb4
luzi82 <luzi82@gmail.com>
2011-04-13 23:31:56 +0800

    stop logging

--- src/com/luzi82/madokacountdown/MadokaCountdown.java
+++ src/com/luzi82/madokacountdown/MadokaCountdown.java
@@ -6,7 +6,12 @@ public class MadokaCountdown {
 
 	public static String TAG = "MadokaCountdown";
 
+	public static final boolean DEBUG = false;
+
 	static void logd(String msg) {
+		if (!DEBUG) {
+			return;
+		}
 		Log.d(TAG, msg);
 	}
 
